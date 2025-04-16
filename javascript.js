f1e7a83f2dd8aff3e7f5334d5698de642fc36e71
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-16 12:45:30 +0200

    Generate the  function to get rock paper scissor
    Create a function that generates a random Number between and including
    1-3, set 1 to rock, 2 to paper and 3 to scissor and check in if
    statement what number was rolled by the computer, return the result

--- javascript.js
+++ javascript.js
@@ -1,1 +1,23 @@
 console.log("Hello World");
+
+const rock = "rock";
+const paper = "paper";
+const scissor = "scissor";
+
+
+function getComputerChoice() {
+    let result = Math.floor(Math.random() * 3 + 1);
+    
+    if (result === 1) {
+        return rock;
+    } 
+    else if (result === 2) {
+        return paper;
+    }
+    else {
+        return scissor; 
+    }
+    
+}
+
+console.log(getComputerChoice());
