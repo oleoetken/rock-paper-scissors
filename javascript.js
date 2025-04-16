1b5d4c6c4d5252675a5e611aa946e9759bc69cb1
oleoetken <104391010+oleoetken@users.noreply.github.com>
2025-04-16 13:47:06 +0200

    Add logic to the game itself
    Create function and variables for the function to use to play the game
    itself. Use if statements to check the humanChoice and computerChoice
    and compare with && operator

--- javascript.js
+++ javascript.js
@@ -4,6 +4,12 @@ const rock = "rock";
 const paper = "paper";
 const scissor = "scissor";
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3 + 1);
@@ -20,7 +26,7 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
+/*console.log(getComputerChoice());*/
 
 function getHumanChoice() {
    let userInput = prompt("Rock, Paper or Scissor?"); 
@@ -39,4 +45,45 @@ function getHumanChoice() {
    }
 }
 
-console.log(getHumanChoice());
+/*console.log(getHumanChoice());*/
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "rock") {
+        return "It's a draw!";
+    } 
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "You lose! Paper beats Rock.";
+    } 
+    else if (humanChoice === "rock" && computerChoice === "scissor") {
+        humanScore++;
+        return "You win! Rock beats Scissor.";
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You win! Paper beats Rock.";
+    }
+    else if (humanChoice === "paper" && computerChoice === "paper") {
+        return "It's a draw!"
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissor") {
+        computerScore++;
+        return "You lose! Scissor beats Paper.";
+    } 
+    else if (humanChoice === "scissor" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats Scissor.";
+    }
+    else if (humanChoice === "scissor" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissor beats Paper.";
+    }
+    else if (humanChoice === "scissor" && computerChoice === "scissor") {
+        return "It's a draw!";
+    }
+    else {
+        return "Something went wrong";
+    }
+}
+/*playRound(humanSelection, computerSelection);*/
+console.log(playRound(humanSelection, computerSelection));
